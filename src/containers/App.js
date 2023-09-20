490bc24bfdf765215b4874c52f84763e04b1d68c
Biediger <cabiediger@gmail.com>
2023-09-20 15:57:55 -0400

    update class component to reach hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,51 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
-class App extends Component {
 
-  constructor() {
-    super()
-    this.state = {
-      robots: [],
-      searchField: ''
-    }
+
+const App = () => {
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState('');
+
+  useEffect(() => {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => setRobots(users))
+  }, [])
+
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value);
   }
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ robots: users }));
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase())
+  })
+
+  const renderLodaingPage = () => {
+    return <h1 className='loading'>Loading</h1>
   }
 
-  onSearchChange = (event) => {
-    this.setState({searchField: event.target.value})
+  const renderRobotsPage = () => {
+    return (
+      <div className="tc">
+        <h1 className='f1 title'>RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange}/>
+          <Scroll>
+            <ErrorBoundry>
+              <CardList robots={filteredRobots}/>
+            </ErrorBoundry>
+          </Scroll>
+      </div>
+    )
   }
 
-  render() {
-    const { robots, searchField } = this.state;
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase())
-    })
-    
-    if (!robots.length) {
-      return <h1 className='loading'>Loading</h1>
-    } else {
-      return (
-        <div className="tc">
-          <h1 className='f1 title'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
-            <Scroll>
-              <ErrorBoundry>
-                <CardList robots={filteredRobots}/>
-              </ErrorBoundry>
-            </Scroll>
-        </div>
-      );
-    }
-  }
+  return !robots.length ? renderLodaingPage() : renderRobotsPage()
 }
 
 export default App;
